736842be41a64633851e02adbb632ce394d2d802
Franco-M-Enzian <emf0000b@gmail.com>
2025-01-17 02:27:09 +0900

    refactor: 不要なimport文の削除

--- src/pages/top.js
+++ src/pages/top.js
@@ -1,15 +1,13 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import '../styles/top.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 /**
  * TOP画面をレンダリングする
  *
  * @function renderTopPage
- * @returns {void} 
+ * @returns {void}
  */
 export function renderTopPage() {
-  const app = document.getElementById('app');
+  const app = document.getElementById("app");
   app.innerHTML = `
   <div class="container mt-5">
       <h1 class="text-primary">TOP画面</h1>
@@ -18,13 +16,15 @@ export function renderTopPage() {
   </div>
   `;
 
-  const startButton = document.getElementById('goToGameButton');
-  startButton.addEventListener('click', () => {
-    import('./game.js').then(module => module.renderGamePage());
+  const startButton = document.getElementById("goToGameButton");
+  startButton.addEventListener("click", () => {
+    import("./game.js").then((module) => module.renderGamePage());
   });
 
-  const instructionButton = document.getElementById('goToInstructionButton');
-  instructionButton.addEventListener('click', () => {
-    import('./instruction.js').then(module => module.renderInstructionsPage());
+  const instructionButton = document.getElementById("goToInstructionButton");
+  instructionButton.addEventListener("click", () => {
+    import("./instruction.js").then((module) =>
+      module.renderInstructionsPage()
+    );
   });
 }
