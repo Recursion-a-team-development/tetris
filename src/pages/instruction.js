1a3d2c3036dd6c0bd5d946e8684cb561c8060a8e
Franco-M-Enzian <emf0000b@gmail.com>
2025-01-17 02:26:06 +0900

    style: フォーマッターの適用

--- src/pages/instruction.js
+++ src/pages/instruction.js
@@ -1,14 +1,14 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 /**
  * 説明画面をレンダリングする
- * 
+ *
  * @function renderInstructionsPage
  * @returns {void}
  */
 export function renderInstructionsPage() {
-  const app = document.getElementById('app');
+  const app = document.getElementById("app");
   app.innerHTML = `
     <div class="container mt-5">
       <h1 class="text-primary">説明画面</h1>
@@ -16,8 +16,8 @@ export function renderInstructionsPage() {
     </div>
   `;
 
-  const backButton = document.getElementById('backToTopButton');
-  backButton.addEventListener('click', () => {
-    import('./top.js').then(module => module.renderTopPage());
+  const backButton = document.getElementById("backToTopButton");
+  backButton.addEventListener("click", () => {
+    import("./top.js").then((module) => module.renderTopPage());
   });
 }
